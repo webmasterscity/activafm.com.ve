14d76e6bc0c28355fd77163b35138081ffe7be9b
webmasterscity <ds000082@gmail.com>
2017-07-28 16:14:11 -0400

    inicio

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="es">
+<head><title>Sistema de Entrenamiento para Competencias Internacionales de Programación</title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+<div style="text-align:center"><img src="images/fondo.jpg"/></div>
+</body> 
+</html>
