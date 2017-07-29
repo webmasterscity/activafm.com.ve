0467cd3457541ae95fa70ab9f31820a81c0cfe96
webmasterscity <ds000082@gmail.com>
2017-07-29 09:58:45 -0400

    links radio sin audio

--- index.php
+++ index.php
@@ -4,8 +4,33 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link type="text/css" rel="stylesheet" href="libreria/bootstrap-3.3.6/css/bootstrap.min.css">
+	<script type="text/javascript" src="libreria/bootstrap-3.3.6/js/bootstrap.min.js"></script>
 </head>
 <body>
-<div style="text-align:center"><img src="images/fondo.jpg"/></div>
+	<div class="row">
+	
+		<div class="col-md-12" style="text-align:center"><img src="images/fondo_1.jpg"/></div>
+		
+	</div>
+	<div class="row">
+		<div class="col-md-3"></div>
+		<div class="col-md-2" style="text-align:center"><img src="images/logo_acarigua.jpg"/></div>
+		<div class="col-md-2" style="text-align:center"><img src="images/logo_intermedio.jpg"/></div>
+		<div class="col-md-2" style="text-align:center"><img src="images/logo_ospino.jpg"/></div>
+		<div class="col-md-3"></div>
+	</div>
+	<div class="row">
+		<div class="col-md-3"></div>
+		<div class="col-md-2" style="text-align:center"><audio autoplay style="width:220px" controls="" preload="none" src=""></audio></div>
+		<div class="col-md-2" style="text-align:center"></div>
+		<div class="col-md-2" style="text-align:center"><audio title="Muy pronto!" autoplay style="width:220px" controls="" preload="none" src=""></audio></div>
+		<div class="col-md-3"></div>
+	</div>
+	<div class="row">
+	
+		<div class="col-md-12" style="text-align:center"><img src="images/fondo_2.jpg"/></div>
+		
+	</div>
 </body> 
 </html>
